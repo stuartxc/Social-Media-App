8e1ca6071151ece58e59939c63977078da28892f
allanx01 <allanx01@students.cs.ubc.ca>
2023-11-27 00:45:00 -0800

    forgot to add file

--- backend/src/controller/Account.js
+++ backend/src/controller/Account.js
@@ -19,34 +19,6 @@ class Account {
     }
   }
 
-  // static async queryDb(queryText) {
-  //   const pool = new Pool({
-  //     user: process.env.DB_USER,
-  //     host: process.env.DB_HOST,
-  //     database: process.env.DB_NAME,
-  //     port: process.env.DB_PORT,
-  //   })
-  //   const client = await pool.connect()
-  //   try {
-  //     const res = await this.client.query(queryText)
-  //     return res.rows
-  //   } finally {
-  //     client.release()
-  //   }
-  // }
-
-  static async getTest(req, res) {
-    // // const db = DatabaseInstance.getInstance()
-    // // const id = req.params.userId
-    // try {
-    //   const data = await this.queryDb(`SELECT * FROM test;`)
-    //   res.json(data)
-    // } catch (error) {
-    //   console.error(error)
-    //   res.status(500).send("Server Error")
-    // }
-  }
-
   // update an account
   static async update(req, res, next) {}
 }
