3606994f347821036c1e21a49f4d9494272e802e
stuartcc <stuartcc@students.cs.ubc.ca>
2023-11-27 01:05:33 -0800

    bimmy baw caption

--- backend/src/controller/Post.js
+++ backend/src/controller/Post.js
@@ -1,13 +1,17 @@
-import Caption from ".Caption.js"
+const Caption = require("./Caption.js");
 
+const DatabaseInstance = require("../database/Database");
 var format = require("pg-format");
 
 class Post {
+	db = DatabaseInstance.getInstance()
+
     static async create(req, res) {
+		
 		try {
 			const text = "INSERT INTO Post(postID, URL, caption, createdBy, type) VALUES($1, $2, $3, $4, $5) RETURNING *"
 			const values = [1, "placeholderURL", "test caption", "stuart", 0]
-			const data = await this.queryDatabase(text, values)
+			const data = await db.queryDb(text, values)
 			console.log(res.json(data.rows[0]))
 			Caption.create(req, res)
 		  } catch (error) {
@@ -19,7 +23,7 @@ class Post {
 		try {
 			const target = req.params.id
 			const sql = format("DELETE FROM Post WHERE postID = %L", target)
-			const data = await this.queryDatabase(sql)
+			const data = await db.queryDb(sql)
 			console.log(res.json(data.rows[0]))
 		  } catch (error) {
 			console.error(error)
@@ -28,7 +32,7 @@ class Post {
 	}
     static async get(req, res) {
 		try {
-			const data = await this.queryDatabase("SELECT postID FROM Post;")
+			const data = await db.queryDb("SELECT postID FROM Post;")
 			console.log(res.json(data));
 		  } catch (error) {
 			console.error(error)
